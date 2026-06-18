5ce98df4de21e02e053ff9c69f6041bb8e4df208
Harsh-Kumar-Mishra <mishrakumarharsh987@gmail.com>
2026-06-18 18:51:21 +0530

    fixed folder and file strucute

--- src/pages/bookAppointments/bookAppointements.tsx
+++
@@ -1,7 +0,0 @@
-import BookAppointmentForm from "../../components/form/BookAppointmentForm";
-
-const BookAppointment = () => {
-  return <BookAppointmentForm />;
-};
-
-export default BookAppointment;
